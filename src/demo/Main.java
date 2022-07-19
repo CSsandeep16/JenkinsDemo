e573ac8465e7663a3321d8c556fff08b4591ec1b
unknown <eyjava08@iiht.tech>
2022-07-19 22:53:45 +0530

    Initial Commit

---
+++ src/demo/Main.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World!");
+        System.out.println("This is a jenkins project");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
